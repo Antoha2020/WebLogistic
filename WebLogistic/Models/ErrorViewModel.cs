4bb10150ce86bc1e886e401bdaaebb6cc3134c56
anton <kolesnyk.ant@gmail.com>
2021-03-10 09:20:49 +0200

    Добавьте файлы проекта.

---
+++ WebLogistic/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebLogistic.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
